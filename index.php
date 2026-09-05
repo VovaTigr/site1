91ad98a5f8d19a60aafc3702562704a55803a58e
Vov4ik <vovasviridenko929@gmail.com>
2026-09-05 21:02:46 +0300

    Add initial PHP configuration and launch settings for debugging

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$x = 5;
+$y = 10;
+$sum = $x + $y;
+// echo $sum; 
+$year = 2026; // number(integer)
+$is_hungry = false; // boolean
+$price = 99.99; // float
+$favourite_movie = "Charlie and the Chocolate Factory"; // string
+define('FAVOURITE_GAME', 'Limbus Company');
+const FAVOURITE_BOOK = 'Odyssey';
+echo "My favourite game is " . FAVOURITE_GAME . "\n" . " and my favourite book is " . FAVOURITE_BOOK . ".";
