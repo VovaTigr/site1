e229ee7d80833f58a96818ec11719f7639d1ea9a
Vov4ik <vovasviridenko929@gmail.com>
2026-09-06 20:38:27 +0300

    3.7 Done

--- homework1.php
+++ homework1.php
@@ -109,14 +109,36 @@ echo ($body_temp > 37.5 ? "<p>You're too hot! You should stay at home drink wate
 
 // 3.5 - The Area of a Circle
 
-$circle = rand(1, 20) * 100;
+$circle = rand(1, 20) * 10;
 $radius = $circle / 2;
 define("PI", 3.14);
 $area = PI * $radius * $radius;
 
-echo "<div class='circle' style='border-radius: 50%; border: solid 2px black; background-color: transparent; width: 200px; height: 200px;'>
-        <hr style='border: none; border-top: solid 2px black; width: 100px; margin: 0; position: relative; top: 50%; left: 0; transform: translateY(-50%);'>
+echo "<div class='circle' style='border-radius: 50%; border: solid 2px black; background-color: transparent; width: $circle px; height: $circle px;'>
+        <hr style='border: none; border-top: solid 2px black; width: $radius px; margin: 0; position: relative; top: 50%; left: 0; transform: translateY(-50%);'>
     </div>;\n";
 echo "<hr>\n";
-echo "The Radius of the circle is " . $radius . " pixels \n";
-echo "The Area of the circle is " . $area . " square pixels";
+echo "<p>The Radius of the circle is " . $radius . " pixels </p>\n";
+echo "<p>The Area of the circle is " . $area . " squared pixels</p>\n";
+
+//3.7 - The School tasks
+
+$school_task = round(rand(1, 13));
+echo "<p>Today's school task is: </p>\n";
+
+switch ($school_task) {
+    case 1: echo "<p></p>Study Mathematics</p>"; break;
+    case 2: echo "<p>Study Science</p>"; break;
+    case 3: echo "<p>Study English</p>"; break;
+    case 4: echo "<p>Study History</p>"; break;
+    case 5: echo "<p>Study Geography</p>"; break;
+    case 6: echo "<p>Study Art</p>"; break;
+    case 7: echo "<p>Study Music</p>"; break;
+    case 8: echo "<p>Study Physical Education</p>"; break;
+    case 9: echo "<p>Study Computer Science</p>"; break;
+    case 10: echo "<p>Study Foreign Language</p>"; break;
+    case 11: echo "<p>Study Economics</p>"; break;
+    case 12: echo "<p>Study Psychology</p>"; break;
+    case 13: echo "<p>Study Philosophy</p>"; break;
+    default: echo "<p>You aren't even allowed to study</p>"; break;
+}
