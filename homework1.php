84aa174f9b2c71a1bd68047bc4a96c8ff49501f0
Vov4ik <vovasviridenko929@gmail.com>
2026-09-06 19:55:23 +0300

    3.4 Done

--- homework1.php
+++ homework1.php
@@ -80,22 +80,29 @@ $body_close = <<<EOD
 </html>
 EOD;
 echo $head . "\n" .$body_open . "\n" . $nav_menu . "\n" . $header . "\n" . $main . "\n" . $footer . "\n" . $body_close . "\n";
-echo "<hr>";
+echo "<hr>\n";
 
-// 3.3 - Temperature
+// 3.3 - Temperature Outside
 $temp = round(rand(-20, 50));
-echo "It's " . $temp . " degrees outside";
+echo "<p>It's " . $temp . " degrees outside</p>\n";
 if ($temp >= -20 && $temp <= 0) {
-    echo "<p>This is freezing! Won't even gonna go outside!</p>";
+    echo "<p>This is freezing! Won't even gonna go outside!</p>\n";
 } elseif ($temp >= 1 && $temp <= 10) {
-    echo "<p>It's chilly! Gotta grab a jacket!</p>";
+    echo "<p>It's chilly! Gotta grab a jacket!</p>\n";
 } elseif ($temp >= 11 && $temp <= 20) {
-    echo "<p>It's cool outside! I like it. I shall go for walk!</p>";
+    echo "<p>It's cool outside! I like it. I shall go for walk!</p>\n";
 } elseif ($temp >= 21 && $temp <= 30) {
-    echo "<p>Today is a nice temperature! Let's hang out!!</p>";
+    echo "<p>Today is a nice temperature! Let's hang out!!</p>\n";
 } elseif ($temp >= 31 && $temp <= 40) {
-    echo "<p>Woah! This summer is really cruel. I would rather stay at home</p>";
+    echo "<p>Woah! This summer is really cruel. I would rather stay at home</p>\n";
 } else {
-    echo "<p>I... am... melting...</p>";
+    echo "<p>I... am... melting...</p> \n";
 }
+echo "<hr>\n";
 
+// 3.4 - On a Doctor's Inspection
+$body_temp = rand(350, 400)/10;
+
+echo "<p>Your body temperature is " . $body_temp . " degrees Celsius</p>\n";
+echo ($body_temp < 36.5 ? "<p>You're too cold! You should wear a coat, stay at home and drink hot tea!</p>" : "<p>You're not too cold!</p>");
+echo ($body_temp > 37.5 ? "<p>You're too hot! You should stay at home drink water and stay in bed</p>" : "<p>You're not too hot!</p>");
